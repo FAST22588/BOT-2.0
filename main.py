66e69210d0220d5109fe074c59cbed4a9c8e6d79
FAST22588 <iloveyouv100@gmail.com>
2025-04-19 02:21:26 +0700

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "MTM2MTc0Mzc5NzAzMzM3MzkzOQ.GEj5c-.fvSPrxW01-PcVD_G0vkmUvS47-DjRYHqJmSD3Y"
-
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
