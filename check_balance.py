7a21f4ba4a3db764b63e38253a5200e1e3c1b658
FAST22588 <iloveyouv100@gmail.com>
2025-04-19 16:29:24 +0700

    Update check_balance.py

--- check_balance.py
+++ check_balance.py
@@ -11,7 +11,7 @@ class CheckBalance(commands.Cog):
         self.bot = bot
 
     # /check @user
-    @app_commands.command(name="check", description="ตรวจสอบยอดเงินของผู้ใช้ (Admin เท่านั้นน!)")
+    @app_commands.command(name="check", description="ตรวจสอบยอดเงินของผู้ใช้ในเซิฟเวอร์ (Admin เท่านั้นน!)")
     @app_commands.describe(member="ผู้ใช้ที่ต้องการตรวจสอบยอดเงิน")
     async def check_command(self, interaction: discord.Interaction, member: discord.Member):
         if interaction.channel.id != COMMAND_CHANNEL_ID:
